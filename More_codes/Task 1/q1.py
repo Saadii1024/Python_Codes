cb72bef9e6878c1fa88836a226775b62fd0a129c
Saad Ahmad <saad.ahmad1024@gmail.com>
2021-05-16 17:01:12 +0500

    Add files via upload

---
+++ More_codes/Task 1/q1.py
@@ -0,0 +1,7 @@
+length = int(input("Enter your length: "))
+width  = int(input("Enter your width: "))
+if length == width:
+    print("It is a Square!")
+else:
+    print("It is a Rectangle!")
+    
