834708eb15e548a8d49057e0dfd0565c6127750c
Saad Ahmad <saad.ahmad1024@gmail.com>
2021-05-27 00:51:23 +0500

    Add files via upload

---
+++ More_codes/Task 3/q1.py
@@ -0,0 +1,11 @@
+#importing math library
+import math
+
+#taking value of x from user
+x = int(input("Enter the value of x: "))
+k = 2.5
+
+#calculating value of y
+y = x * 2.5
+
+print("The value of y is:",y)
