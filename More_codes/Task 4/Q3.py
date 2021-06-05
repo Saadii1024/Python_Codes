52761d9b8b04b81b2a74837005d343de9b7e2446
Saad Ahmad <saad.ahmad1024@gmail.com>
2021-06-05 07:11:19 +0500

    Add files via upload

---
+++ More_codes/Task 4/Q3.py
@@ -0,0 +1,9 @@
+print ("*","^","@","-")
+print ("\n")
+def pattern(s,n):
+    for n in range(y):
+        n+=1
+        print(x*n)
+x=input("Enter the symbol: ")
+y=int(input("Enter the number: "))
+pattern(x,y)
