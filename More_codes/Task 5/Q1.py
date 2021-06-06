a6e78338b05955cadd0cb36dffd71633945271a4
Saad Ahmad <saad.ahmad1024@gmail.com>
2021-06-06 21:49:16 +0500

    Add files via upload

---
+++ More_codes/Task 5/Q1.py
@@ -0,0 +1,46 @@
+a = [1,3,5]
+b = [2,4,6]
+c = b+a         #adding two lists
+print(c)        #printing the list "c"
+
+
+
+
+c.insert(3,42)  #inserting 42 in the list
+print(c)
+
+
+
+
+min(c)  #finding the minimum value in the list
+
+
+
+c.append(7)  #appending 7 in the list
+
+c.append(8)  #appending 8 in the list
+
+c.append(9)  #appending 9 in the list
+
+
+
+
+print(c)  #checking appending number if they are present or not
+
+
+
+print(c[0:2]) #printing the first letters of list "c"
+
+
+
+print(b[2]) #printing last element of list "b"
+
+
+
+print(len(a)) #printing the length of list "a"
+
+
+
+
+
+
